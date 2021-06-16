5e477181a76f3ec429ea5c393064894bffe52d4b
kunalbhumik <kchaudhari013@gmail.com>
2021-06-16 17:13:31 +0530

    1 file modified

--- src/app/appPipe/filter.ts
+++ src/app/appPipe/filter.ts
@@ -6,7 +6,11 @@ import {Pipe,PipeTransform} from '@angular/core'
 
 export class FilterPipe implements PipeTransform{
 
-  transform(value:any,searchTerm:any):any{
+  transform(value:any,searchTerm:string):any{
+    
+    if(value.length==0 || searchTerm.length  < 2){
+      return value;
+    }
     return value.filter(function(search){
       return search.name.indexOf(searchTerm) > -1
     })
