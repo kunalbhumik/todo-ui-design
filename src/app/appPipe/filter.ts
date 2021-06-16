af557a260a0a6b8a26a2376119f4a7f25ed5570c
kunalbhumik <kchaudhari013@gmail.com>
2021-06-16 18:52:26 +0530

    2 files modified

--- src/app/appPipe/filter.ts
+++ src/app/appPipe/filter.ts
@@ -9,11 +9,8 @@ export class FilterPipe implements PipeTransform{
 
   
     transform(items: TaskCategory[], filter: string): any {
-        if (!items || !filter) {
-            return items;
-        }
-        // filter items array, items which match and return true will be
-        // kept, false will be filtered out
+        
+        
         return items.filter(item =>item.name && item.name.indexOf(filter) > -1  );
     }
 }
